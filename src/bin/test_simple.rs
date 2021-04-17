2d7facfb2c823a8fcd0ed042ef77d7a46396b1c8
a <a>
2021-04-16 21:25:49 -0400

    Initial Version

--- src/bin/test_simple.rs
+++ src/bin/test_simple.rs
@@ -1,12 +1,36 @@
-use rustmp::rmp_parallel_for;
+use rand::Rng;
+use rustmp::par_for;
+use std::time;
+
+#[derive(Debug)]
+struct Student {
+    name: String,
+    age: u8,
+    gpa: f32,
+}
+
+impl Student {
+    pub fn new(age: u8) -> Student {
+	Student { name: "Default".to_string(),
+		  age: age,
+		  gpa: age as f32 }
+    }
+}
 
 fn main() {
-    #[rmp_parallel_for]
-    fn inner() {
-        for i in 1..10 {
-            println!("Hello from {}!", i);
-        }
-    }
+    let numbers: Vec<Student> = vec![];
 
-    inner();
+    par_for! {
+    for i in 1..10, capturing numbers {
+	std::thread::sleep(
+	    time::Duration::from_secs(
+		rand::thread_rng().gen_range(1..10)));
+	let mut lock = numbers.write();
+	lock.push(Student::new(i));
+	println!("Thread {} running!", i);
+    } };
+
+    for num in numbers {
+	println!("{:?}", num);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,57 @@
-use proc_macro::TokenStream;
+use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
-#[proc_macro_attribute]
-pub fn rmp_parallel_for(args: TokenStream, func: TokenStream) -> TokenStream {
-    func
+pub struct Capture<T> {
+    value: Arc<RwLock<T>>,
 }
 
+impl<T> Capture<T> {
+    pub fn new(inner: T) -> Capture<T> {
+        return Capture {
+            value: Arc::new(RwLock::new(inner)),
+        };
+    }
+
+    pub fn clone(&self) -> Capture<T> {
+        Capture {
+            value: Arc::clone(&self.value),
+        }
+    }
+
+    pub fn read(&self) -> RwLockReadGuard<T> {
+        return self.value.as_ref().read().unwrap();
+    }
+
+    pub fn write(&self) -> RwLockWriteGuard<T> {
+        return self.value.as_ref().write().unwrap();
+    }
+
+    pub fn unwrap(self) -> T {
+        Arc::try_unwrap(self.value)
+            .ok()
+            .and_then(|o| o.into_inner().ok())
+            .expect("Error: reference copied out of loop")
+    }
+}
+
+#[macro_export]
+macro_rules! par_for {
+    (for $name:ident in $iterator:expr, capturing $captured:ident $blk:block) => {
+        use rustmp::Capture;
+        use std::sync::{Arc, RwLock};
+        use std::thread;
+
+        let itr = $iterator;
+        let $captured = Capture::new($captured);
+        let mut handles: Vec<thread::JoinHandle<()>> = vec![];
+        for $name in itr {
+            let $captured = $captured.clone();
+            handles.push(thread::spawn(move || $blk));
+        }
+
+        for handle in handles {
+            handle.join().expect("Thread paniced!");
+        }
+
+        let $captured = $captured.unwrap();
+    };
+}
